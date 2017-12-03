bfc9bdb5ddc7a3f90b18e6a111b59bde8702ca58
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2017-12-03 15:02:26 -0600

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  LeastSlackScheduler: require("./lib/scheduler/scheduler"),
+  Tasks: {
+    Periodic: require("./lib/scheduler/tasks/periodic"),
+    Aperiodic: require("./lib/scheduler/tasks/aperiodic"),
+    Sporadic: require("./lib/scheduler/tasks/sporadic")
+  }
+}
---
+++ test/schedule.js
@@ -0,0 +1,20 @@
+var LeastSlackScheduler = require("../lib/scheduler/scheduler");
+var Periodic = require("../lib/scheduler/tasks/periodic");
+var Sporadic = require("../lib/scheduler/tasks/sporadic");
+
+var helloLogger = () => {
+  console.log("Hello World");
+}
+
+var goodbyeDelegate = () => {
+  console.log("Goodbye");
+}
+
+var helloLoggerTask  = new Periodic({releaseTime:1000, executionTime:1, period: 2000, delegate:helloLogger});
+var goodbyeTask = new Sporadic({executionTime:1, deadlineTime: 2000, delegate:goodbyeDelegate});
+var leastScheduler = new LeastSlackScheduler();
+
+leastScheduler.addTask("helloLogger", helloLoggerTask);
+leastScheduler.addTask("goodbye", goodbyeTask);
+
+leastScheduler.releaseJob("goodbye");
