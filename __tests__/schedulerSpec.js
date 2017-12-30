c00c21fe18d516f1a7b57b6fb88ba8f35614873d
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2017-12-30 13:15:48 -0600

    releasejob will now return a promise

--- __tests__/schedulerSpec.js
+++ __tests__/schedulerSpec.js
@@ -1,6 +1,7 @@
 
 var LeastSlackScheduler = require("../index").LeastSlackScheduler;
 var Periodic = require("../index").Tasks.Periodic;
+var Aperiodic = require("../index").Tasks.Aperiodic;
 
 test("must construct", () => {
   var scheduler = new LeastSlackScheduler({maxConcurrent:4});
@@ -24,4 +25,14 @@ test("must be able to remove periodic task", () => {
   expect(scheduler.tasks["period"]).not.toBe(null);
   scheduler.removeTask("period");
   expect(scheduler.tasks["period"]).toBe(undefined);
+});
+
+test("must take aperiodic tasks", () => {
+  var scheduler = new LeastSlackScheduler({maxConcurrent:4});
+  var period  = new Aperiodic({executionTime:1, delegate:() => {return 2}});
+  scheduler.addTask("aperiod", period);
+  return scheduler.releaseJob("aperiod").then((data) => {
+    expect(data).toBe(2);
+  })
+
 });
